067ec69b823c82ada40f8c993e92be7fba970a5a
Moophie <michael.van.lierde@hotmail.com>
2021-08-29 00:07:54 +0200

    feat: login and signup frontend

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 // JWT authentication example, use on other routes than index
-app.use('/', passport.authenticate('jwt', {session: false}), indexRouter);
+// app.use('/', passport.authenticate('jwt', {session: false}), indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // Catch 404 and forward to error handler
---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+let signupButton = document.querySelector('#signupButton').addEventListener("click", (e) => {
+    let username = document.querySelector('#username').value;
+    let password = document.querySelector('#password').value;
+
+    fetch("/users/signup", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if (json.status === "success") {
+            let token = json.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "index.html";
+        } else {
+            // Add error handling
+        }
+    })
+});
