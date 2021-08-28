c47bb96fe4ad3220d28242390358a94516013617
Moophie <michael.van.lierde@hotmail.com>
2021-08-28 22:54:21 +0200

    build: install cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const passport = require('./passport/passport');
 
@@ -26,6 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 // JWT authentication example, use on other routes than index
 app.use('/', passport.authenticate('jwt', {session: false}), indexRouter);
 app.use('/users', usersRouter);
