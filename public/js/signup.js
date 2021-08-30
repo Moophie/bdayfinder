e2bbb52f0a597a876a81b36d6705505bbce54114
Moophie <michael.van.lierde@hotmail.com>
2021-08-30 18:13:46 +0200

    feat: registration no longer needs a thomasmore email

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,32 +6,29 @@ let signupButton = document.querySelector('#signupButton').addEventListener("cli
     let birthday = document.querySelector('#birthday').value;
     let password = document.querySelector('#password').value;
 
-    if (email.endsWith("@student.thomasmore.be")) {
-        fetch("/users/signup", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "username": username,
-                "email": email,
-                "firstname": firstname,
-                "lastname": lastname,
-                "birthday": birthday,
-                "password": password,
-            })
-        }).then(response => {
-            return response.json();
-        }).then(json => {
-            if (json.status === "success") {
-                let token = json.data.token;
-                localStorage.setItem("token", token);
-                window.location.href = "chat.html?birthday=" + json.data.currentBirthday;
-            } else {
-                // Add error handling
-            }
+    fetch("/users/signup", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": username,
+            "email": email,
+            "firstname": firstname,
+            "lastname": lastname,
+            "birthday": birthday,
+            "password": password,
         })
-    } else {
-        // Error handling
-    }
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if (json.status === "success") {
+            let token = json.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "chat.html?birthday=" + json.data.currentBirthday;
+        } else {
+            // Add error handling
+        }
+    })
+
 });
